8e82ce90705919a3a7f1681e4f22f1387364b635
Madele Theron <59708219+Madele-theron@users.noreply.github.com>
2022-09-20 23:52:42 +0200

    Create task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - Checks if a singly-linked list contains a cycle.
+ * @list: A singly-linked list.
+ *
+ * Return: 1 if the list has a cycle, 0 if it doesn't
+ */
+int check_cycle(listint_t *list)
+{
+    listint_t *slow, *fast;
+
+    if (list == NULL || list->next == NULL)
+        return (0);
+
+    slow = list->next;
+    fast = list->next->next;
+
+    while (slow && fast && fast->next)
+    {
+        if (slow == fast)
+            return (1);
+
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return (0);
+}
