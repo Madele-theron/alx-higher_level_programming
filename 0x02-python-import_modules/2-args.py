249decdd3596600331c776e4de085f877acb9f61
Madele <mtheron@ssi.engineering>
2022-11-28 20:47:24 +0200

    made file executable with chmod u+x

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" the code should not execute when imported  """
+
 if __name__ == "__main__":
     """prints the number of and the list of its arguments."""
     import sys
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif args == 1:
         print("1 argument:")
-    elif args > 1:
+    else:
         print("{} arguments:".format(args))
     for item in range(args):
         print("{}: {}".format(item + 1, sys.argv[item + 1]))
