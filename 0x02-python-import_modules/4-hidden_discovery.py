4589f11cabb43b081597f67b23c2e8d5c7400d82
Madele <mtheron@ssi.engineering>
2022-12-06 20:41:39 +0200

    task 5 again

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,5 @@
 # !/usr/bin/python3
 
-# import struct
-
-
 # if __name__ == "__main__":
 #     """Print all names defined by hidden_4 module."""
 #     import hidden_4
@@ -10,9 +7,6 @@
 #     names = dir(hidden_4)
 #     print(names)
 
-
-#     with open('hidden_4.pyc', 'rb') as f:
-#         print struct.unpack('<H', f.read(2))
 
 if __name__ == "__main__":
     """Print all names defined by hidden_4 module."""
@@ -22,3 +16,5 @@ if __name__ == "__main__":
     for name in names:
         if name[:2] != "__":
             print(name)
+
+# verstaan dit nie.
